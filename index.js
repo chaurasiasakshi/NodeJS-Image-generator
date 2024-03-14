6679ee04dd4c31ee7c3b44f0efff56eda384b77d
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-03-14 22:20:51 +0530

    Create Frontend

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require("express")
-const dotenv = require("dotenv").config();
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-port  = process.env.PORT || 5000;
+const app = express();
 
-app=express();
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/openai',require('./routes/openaiRoutes'));
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/openai', require('./routes/openaiRoutes'));
 
-app.listen(port,()=>{
-    console.log(`Server started on port ${port}`);
-})
+app.listen(port, () => console.log(`Server started on port ${port}`));
